b66a31df7f608ffe402ce47e05c5687c9ecfc48e
sarah_olvr <olvr1.sarah@gmail.com>
2020-11-02 15:36:44 +0100

    fisrt commit

---
+++ src/com/exos/Main.java
@@ -0,0 +1,16 @@
+package com.exos;
+
+import java.util.*
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int saisiNb;
+        System.out.println("Choisir un nombre entre 1 et 100:");
+        Scanner saisiNb = new Scanner(System.in);
+        saisiNb = 
+
+
+    }
+}
