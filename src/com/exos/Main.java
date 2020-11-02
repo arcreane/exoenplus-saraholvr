1b96911a26dd3d49ff10a5337cee584ed35c24e9
sarah_olvr <olvr1.sarah@gmail.com>
2020-11-02 22:39:23 +0100

    première partie de l'exo 1

--- src/com/exos/Main.java
+++ src/com/exos/Main.java
@@ -1,16 +1,30 @@
 package com.exos;
 
-import java.util.*
+import java.util.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
         int saisiNb;
+        int i;
+        int random;
+        int min = 1;
+        int max = 100;
         System.out.println("Choisir un nombre entre 1 et 100:");
-        Scanner saisiNb = new Scanner(System.in);
-        saisiNb = 
+        Scanner lectureClavier = new Scanner(System.in);
+        saisiNb = lectureClavier.nextInt();
 
-
+        for (i = 0; i < 10; i++) {
+            if (saisiNb == Math.random() * (max - min)) {
+                System.out.println("le nombre mystere est: " + saisiNb);
+            } else {
+                System.out.println("ce n'est pas le bon");
+            }
+        }
+        while ( saisiNb != random);
+        {
+            
+        }
     }
 }
